22c1c15fc8c9e1f763ecd787e621729eb6dd5612
Rfsosnow <rhys.soz@gmail.com>
2017-10-13 10:59:56 -0600

    Changed functionality to be more universal
    Extracted and moved methods from the main class over to this one (sumArray and created rollFourDropLowest). Removed the unnecessary sides instance variable, and deleted the references to it throughout. Deleted constructors that no longer made sense. Changed roll function to dynamically take a number of sides for every roll.

--- nSidedDie.java
+++ nSidedDie.java
@@ -2,45 +2,44 @@ import java.util.Random;
 
 
 public class nSidedDie {
-	private int sides;
 	public Random diceRoller;
 	
-	public nSidedDie() {
-		this(6);		//default to six sided die if no other die given
+	public nSidedDie(){
+		diceRoller = new Random();
 	}
 	
-	public nSidedDie(int inputSides){
-		if (inputSides < 1){
-			throw new IllegalArgumentException("not enough sides");
-		} else{
-			sides = inputSides;
-			diceRoller = new Random();
+	public nSidedDie(long seed){
+		diceRoller = new Random(seed);
+	}
+	
+	public int roll(int sides){
+		if(sides<1){
+			throw new IllegalArgumentException();
 		}
-	}
-	
-	public nSidedDie(int inputSides, long seed){
-		if (inputSides < 1){
-			throw new IllegalArgumentException("not enough sides");
-		} else{
-			sides = inputSides;
-			diceRoller = new Random(seed);
-		}
-	}
-	
-	public void setSides(int inputSides){
-		if (inputSides < 1){
-			throw new IllegalArgumentException("not enough sides");
-		} else{
-			sides = inputSides;
-		}
-	}
-	
-	public int getSides(){
-		return sides;
-	}
-	
-	public int roll(){
 		return (diceRoller.nextInt(sides)+1);
 	}
 	
+	public int rollFourDropLowest(int sides){
+		int[] rollArray = new int[4];
+		int lowestRoll = sides+1;
+		int lowestRollLocation = 0;
+		for(int x = 0;x<4;x++){
+			rollArray[x] = (diceRoller.nextInt(sides)+1);
+			if(rollArray[x] < lowestRoll){
+				lowestRoll = rollArray[x];
+				lowestRollLocation = x;
+			}
+		}
+		rollArray[lowestRollLocation] = 0;
+		return sumArray(rollArray);
+	}
+	
+	public static int sumArray(int[] array){
+		int returnInt = 0;
+		for (int x = 0; x < array.length;x++){
+			returnInt += array[x];
+		}
+		return returnInt;
+	}
+	 
 }
